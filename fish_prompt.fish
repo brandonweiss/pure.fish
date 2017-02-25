17614152954a14f3ccab5362de492851d3178e40
Tyler Funnell <tfunnell@bccrc.ca>
2017-02-25 10:41:55 -0800

    Adding printing of command duration

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -45,6 +45,47 @@ function _git_upstream_status
   echo $arrows
 end
 
+function _cmd_duration_days
+  set -l days (expr $CMD_DURATION / 1000 / 60 / 60 / 24)
+  if test $days -gt 0
+    echo $days"d"
+  end
+end
+
+function _cmd_duration_hours
+  set -l hours (expr $CMD_DURATION / 1000 / 60 / 60 \% 24)
+  if test $hours -gt 0
+    echo $hours"h"
+  end
+end
+
+function _cmd_duration_mins
+  set -l minutes (expr $CMD_DURATION / 1000 / 60 \% 60)
+  if test $minutes -gt 0
+    echo $minutes"m"
+  end
+end
+
+function _cmd_duration_secs
+  set -l seconds (expr $CMD_DURATION / 1000 \% 60)
+  if test $seconds -gt 0
+    echo $seconds"s"
+  end
+end
+
+function _cmd_duration
+  set -l duration ""
+
+  if test (expr $CMD_DURATION / 1000) -gt 5
+    set duration "$duration"(_cmd_duration_days)
+    set duration "$duration"(_cmd_duration_hours)
+    set duration "$duration"(_cmd_duration_mins)
+    set duration "$duration"(_cmd_duration_secs)
+  end
+
+  echo $duration
+end
+
 function _print_in_color
   set -l string $argv[1]
   set -l color  $argv[2]
@@ -71,6 +112,8 @@ function fish_prompt
     _print_in_color " "(_git_branch_name_or_revision) 242
     _print_in_color " "(_git_upstream_status) cyan
   end
+
+  _print_in_color " "(_cmd_duration) yellow
 
   _print_in_color "\n❯ " (_prompt_color_for_status $last_status)
 end
