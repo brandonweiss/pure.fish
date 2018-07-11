497aa04c4264b298d3eb39c8968b6ac8549bace3
Will Stone <654103+will-stone@users.noreply.github.com>
2018-07-11 11:33:46 +0100

    remove git equal symbol, change git diverged symbol

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -10,8 +10,10 @@ set __fish_git_prompt_color_upstream cyan
 set __fish_git_prompt_char_dirtystate '*'
 set __fish_git_prompt_char_stagedstate '⇢'
 set __fish_git_prompt_char_upstream_prefix ' '
+set __fish_git_prompt_char_upstream_equal ''
 set __fish_git_prompt_char_upstream_ahead '⇡'
 set __fish_git_prompt_char_upstream_behind '⇣'
+set __fish_git_prompt_char_upstream_diverged '⇡⇣'
 
 function _pwd_with_tilde
   echo $PWD | sed 's|^'$HOME'\(.*\)$|~\1|'
