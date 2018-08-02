c1a40e946ace8c12b3f20d47cb059f557e95f8e4
Will Stone <654103+will-stone@users.noreply.github.com>
2018-08-02 04:22:33 +0100

    Use fish_git_prompt (#8)
    * Use fish_git_prompt 
    
    fish has an inbuilt git prompt and can be configured to look like Pure. This simplifies the file by the a long way.
    
    * remove git equal symbol, change git diverged symbol

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,63 +1,22 @@
+# Git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch 242
+set __fish_git_prompt_color_dirtystate FCBC47
+set __fish_git_prompt_color_stagedstate green
+set __fish_git_prompt_color_upstream cyan
+
+# Git Characters
+set __fish_git_prompt_char_dirtystate '*'
+set __fish_git_prompt_char_stagedstate '⇢'
+set __fish_git_prompt_char_upstream_prefix ' '
+set __fish_git_prompt_char_upstream_equal ''
+set __fish_git_prompt_char_upstream_ahead '⇡'
+set __fish_git_prompt_char_upstream_behind '⇣'
+set __fish_git_prompt_char_upstream_diverged '⇡⇣'
+
 function _pwd_with_tilde
   echo $PWD | sed 's|^'$HOME'\(.*\)$|~\1|'
-end
-
-function _in_git_directory
-  git rev-parse --git-dir > /dev/null 2>&1
-end
-
-function _git_branch_name_or_revision
-  set -l branch (git symbolic-ref HEAD ^ /dev/null | sed -e 's|^refs/heads/||')
-  set -l revision (git rev-parse HEAD ^ /dev/null | cut -b 1-7)
-
-  if test (count $branch) -gt 0
-    echo $branch
-  else
-    echo $revision
-  end
-end
-
-function _git_upstream_configured
-  git rev-parse --abbrev-ref @"{u}" > /dev/null 2>&1
-end
-
-function _git_behind_upstream
-  test (git rev-list --right-only --count HEAD...@"{u}" ^ /dev/null) -gt 0
-end
-
-function _git_ahead_of_upstream
-  test (git rev-list --left-only --count HEAD...@"{u}" ^ /dev/null) -gt 0
-end
-
-function _git_dirty
-  set -l is_git_dirty (command git status --porcelain --ignore-submodules ^/dev/null)
-  test -n "$is_git_dirty"
-end
-
-function _git_upstream_status
-  set -l arrows
-
-  if _git_upstream_configured
-    if _git_behind_upstream
-      set arrows "$arrows⇣"
-    end
-
-    if _git_ahead_of_upstream
-      set arrows "$arrows⇡"
-    end
-  end
-
-  echo $arrows
-end
-
-function _git_status
-  set -l asterisk
-
-  if _git_dirty
-    set asterisk "$asterisk*"
-  end
-
-  echo $asterisk
 end
 
 function _print_in_color
@@ -82,11 +41,7 @@ function fish_prompt
 
   _print_in_color "\n"(_pwd_with_tilde) blue
 
-  if _in_git_directory
-    _print_in_color " "(_git_branch_name_or_revision) 242
-    _print_in_color " "(_git_status) FCBC47
-    _print_in_color " "(_git_upstream_status) cyan
-  end
+  __fish_git_prompt " %s"
 
   _print_in_color "\n❯ " (_prompt_color_for_status $last_status)
 end
